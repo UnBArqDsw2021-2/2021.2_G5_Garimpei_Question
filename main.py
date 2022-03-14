61e07385435315e7305b6f136bfed653b461af57
matheusafonsouza <matheusafonsouza@gmail.com>
2022-03-14 20:02:05 -0300

    feat: enable cors on application

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.check import router as check_router
 from common.database import engine
+from common.settings import ORIGINS
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(check_router)
 
